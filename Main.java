f5a47d06143a9852b0b5703b6327799f5651b2f0
Tanmayi <127963666+55-99@users.noreply.github.com>
2023-09-17 20:40:03 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,40 +1,5 @@
-pipeline {
-	agent any
-	stages {
-		stage('One') {
-			steps {
-				echo 'Hi, this is Soumitra from roytuts'
-			}
-		}
-		
-		stage('Two') {
-			steps {
-				input('Do you want to proceed?')
-			}
-		}
-		
-		stage('Build') {
-            steps {
-                bat './gradlew build'
-            }
-        }
-        
-        stage('Test') {
-            steps {
-                bat './gradlew test'
-            }
-        }
-        
-        stage('Check') {
-            steps {
-                bat './gradlew check'
-            }
-        }      
-		
-		stage('Five') {
-			steps {
-				echo 'Finished'
-			}
-		}		
+class Main(){
+	public static void main(String args[]){
+		System.out.println("Hello World");
 	}
 }
